0cce502e141253b1a75e48e5d20446a57dd6e540
Jhonatan Fernando <jhonatanfernando@yahoo.com.br>
2019-07-21 17:01:14 -0300

    Changing the model

--- SOLID/Solid.Core/FizzBuzz.cs
+++ SOLID/Solid.Core/FizzBuzz.cs
@@ -12,6 +12,9 @@ namespace Solid.Core
             if (number % 3 == 0)
                 return "Fizz";
 
+            if (number == 5)
+                return "Buzz";
+
             return number.ToString();
         }
     }
--- SOLID/Solid.Test/Test.cs
+++ SOLID/Solid.Test/Test.cs
@@ -21,5 +21,13 @@ namespace Solid.Test
             var result = fizzBuzz.ReturnValue(4);
             Assert.AreEqual("4", result);
         }
+
+        [Test()]
+        public void Using_Number_5_Should_Print_Buzz()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.ReturnValue(5);
+            Assert.AreEqual("Buzz", result);
+        }
     }
 }
