19c69432b1165ee73b9e2eb45421f65024a5f62c
Jhonatan Fernando <jhonatanfernando@yahoo.com.br>
2019-07-21 12:36:08 -0300

    Creating the model

---
+++ SOLID/Solid.Core/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Solid.Core
+{
+    public class FizzBuzz
+    {
+        public FizzBuzz()
+        {
+        }
+
+        public string ReturnValue(int number)
+        {
+            return "Fizz";
+        }
+    }
+}
---
+++ SOLID/Solid.Test/Test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Solid.Core;
+using System;
+namespace Solid.Test
+{
+    [TestFixture()]
+    public class FizzBuzzTest
+    {
+        [Test()]
+        public void Using_Number_3_Should_Print_Fizz()
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.ReturnValue(3);
+            Assert.AreEqual("Fizz", result);
+        }
+    }
+}
